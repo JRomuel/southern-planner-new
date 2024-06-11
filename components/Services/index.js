c33cd36578ac9eabfb5bb77b1f2fec11ab31f49b
JRomuel <johnromuelmendoza@yahoo.com>
2024-06-11 17:39:15 +0800

    update responsive

--- components/Services/index.js
+++ components/Services/index.js
@@ -13,22 +13,22 @@ export default function Services() {
         <section className={`${styles.section} section`}>
             <div className="container">
                 <div className={styles.contents}>
-                    <div ref={container1} className={styles.l_content}>
-                        <div className={styles.image_wrapper}>
-                            <ParallaxImage src={service_image_2} container={container1}/>
-                        </div>
-                    </div>
+             
                     <div className={styles.r_content}>
                         <div ref={container2} className={styles.image_wrapper}>
                             <ParallaxImage src={service_image_1} container={container2} type="square" label='Services' label_position='right'/>
                         </div>
-                            
+                    </div>
+
+                    <div ref={container1} className={styles.l_content}>
+                        <div className={styles.image_wrapper}>
+                            <ParallaxImage src={service_image_2} container={container1}/>
+                        </div>
                         <div className={styles.text_content}>
                             <h3 className={`${styles.title} section-title`}>We Gotcha Covered!</h3>
                             <div className={styles.comment}>
                                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, repudiandae commodi labore officiis aut expedita quae explicabo impedit rem quos eos tempore voluptate mollitia eligendi temporibus ipsam nihil iusto harum!</p>
                             </div>
-                    
                         </div>
                     </div>
                 </div>
