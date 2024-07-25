aca42d12aec1fa57b48ff8e72c8559e8d73d8751
JRomuel <johnromuelmendoza@yahoo.com>
2024-07-25 22:06:47 +0800

    add partners

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import styles from './footer.module.scss'
 import { AiFillFacebook, AiFillInstagram  } from "react-icons/ai";
-
+import Image from "next/image";
+import partners_balar from "../../public/images/partner-balar.png"
+import partners_raza from "../../public/images/partner-raza.png"
+import partners_rrc from "../../public/images/partner-rrc.png"
 
 export default function Footer() {
   return (
     <footer className={styles.section}>
+      <div className={styles.partners}>
+          <div className="container">
+            <div className={styles.wrapper}>
+              <p className={styles.title}>Partners:</p>
+              <div className={styles.images}>
+                <Image 
+                  className={styles.image}
+                  src={partners_balar} 
+                  placeholder="blur"
+                  alt="Balara Hotel & Spa"/>
+                <Image 
+                  className={styles.image}
+                  src={partners_raza} 
+                  placeholder="blur"
+                  alt="RAZA Stills & Motions"/>
+                <Image 
+                  className={styles.image}
+                  src={partners_rrc} 
+                  placeholder="blur"
+                  alt="RRC Professional Lights & Sounds"/>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="container">
             <div className={styles.content_wrapper}>
                 <div className={styles.brand}>
@@ -25,7 +52,7 @@ export default function Footer() {
                     <div className={styles.contact_details}>
                       <p className="mb-4"><strong>Follow</strong></p>
                       <ul>
-                        <li><a href=""><AiFillFacebook/></a></li>
+                        <li><a href="https://www.facebook.com/southernplanners"><AiFillFacebook/></a></li>
                         <li><a href=""><AiFillInstagram/></a></li>
                       </ul>
                     </div>
