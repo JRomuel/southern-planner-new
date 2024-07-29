ed53247f4e1ea3f992e657179f0a0e7378d20724
JRomuel <johnromuelmendoza@yahoo.com>
2024-07-29 21:00:17 +0800

    updates

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -9,36 +9,41 @@ import partners_rrc from "../../public/images/partner-rrc.png"
 export default function Footer() {
   return (
     <footer className={styles.section}>
-        <div className="container">
-            <div className={styles.content_wrapper}>
-                <div className={styles.brand}>
-                    <p className={styles.title}>Southern Planners</p>
-                    <p className={styles.sub_text}>Event Planner and Stylist</p>
+        <div className={styles.sticky}>
+          <div className={styles.stick_wrapper}>
+              <div className="container">
+                <div className={styles.content_wrapper}>
+                    <div className={styles.brand}>
+                        <p className={styles.title}>Southern Planners</p>
+                        <p className={styles.sub_text}>Event Planner and Stylist</p>
+                    </div>
+                    <div className={styles.contact_wrapper}>
+                        <div className={styles.contact_details}>
+                          <p className="mb-4"><strong>Address</strong></p>
+                          <p>Buliasnin, Boac, Marinduque</p>
+                        </div>
+                        <div className={styles.contact_details}>
+                          <p className="mb-4"><strong>Contact</strong></p>
+                          <p>+63 9631234123</p>
+                          <p><a href="mailto:hecelmariemendoza@gmail.com">hecelmariemendoza@gmail.com</a></p>
+                        </div>
+                        <div className={styles.contact_details}>
+                          <p className="mb-4"><strong>Follow</strong></p>
+                          <ul>
+                            <li><a href="https://www.facebook.com/southernplanners"><AiFillFacebook/></a></li>
+                            <li><a href=""><AiFillInstagram/></a></li>
+                          </ul>
+                        </div>
+                    </div>
+
                 </div>
-                <div className={styles.contact_wrapper}>
-                    <div className={styles.contact_details}>
-                      <p className="mb-4"><strong>Address</strong></p>
-                      <p>Buliasnin, Boac, Marinduque</p>
-                    </div>
-                    <div className={styles.contact_details}>
-                      <p className="mb-4"><strong>Contact</strong></p>
-                      <p>+63 9631234123</p>
-                      <p><a href="mailto:hecelmariemendoza@gmail.com">hecelmariemendoza@gmail.com</a></p>
-                    </div>
-                    <div className={styles.contact_details}>
-                      <p className="mb-4"><strong>Follow</strong></p>
-                      <ul>
-                        <li><a href="https://www.facebook.com/southernplanners"><AiFillFacebook/></a></li>
-                        <li><a href=""><AiFillInstagram/></a></li>
-                      </ul>
-                    </div>
-                </div>
+            </div>
+            <div className={styles.bottom_spacer}>
 
             </div>
+          </div>
         </div>
-        <div className={styles.bottom_spacer}>
 
-        </div>
     </footer>
   )
 }
